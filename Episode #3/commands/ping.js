9196a1c65298ae6731a85f878ecaee30f65c7175
HyRo <generalkubo@gmail.com>
2021-06-21 17:06:45 +0200

    Delete Episode #3 directory

--- Episode #3/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "pong",
-    guildOnly: "833628077556367411",
-    run: async({client, respond, edit}, args) => {
-        respond("pong!")
-    }
-}
