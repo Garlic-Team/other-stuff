10eeeb28def327ee9e77584ec7e1e4754e54f344
HyRo <generalkubo@gmail.com>
2021-06-21 17:10:36 +0200

    Update and rename display-code.ino to pinger.sh

---
+++ pinger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# It's not ddos tool! Education purposes only.
+# This generates a file every 5 minutes
+
+while true; do
+ping example.com
+sleep 2
+# Every 2 seconds it will ping.
+done
