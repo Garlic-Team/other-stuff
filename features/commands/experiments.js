38d0b284ea6d92fb51f05de8261e6b4c2c3a6875
HyRo <generalkubo@gmail.com>
2021-06-25 16:55:29 +0200

    Update experiments.js

--- features/commands/experiments.js
+++ features/commands/experiments.js
@@ -3,10 +3,32 @@ const hash = require("murmurhash")
 module.exports = {
     name: "experiments",
     description: "position",
-    guildOnly: "747526604116459691",
-    run: async({client, member, respond}) => {
-        let position = hash.v3(`2021-05_custom_profiles_premium:${member.id}`) % 1e4
+    guildOnly: "833628077556367411",
+    expectedArgs: "<member:6:member>",
+    run: async({client, member, guild, respond}, args) => {
+        let argMember = args.length != 0 ? guild.members.cache.get(args[0]) : member
+        if(!argMember) argMember = {
+          id: args[0],
+          user: {
+            tag: args[0]
+          }
+        }
 
-        respond(`Profile Banner/About Me **[BETA]**\n\nYour position is **\`${position}\`**`)
+        let bannerColorPicker = hash.v3(`2021-06_banner_color_picker:${argMember.id}`) % 1e4
+
+        let perGuildAvatars = hash.v3(`2021-05_per_guild_avatars:${argMember.id}`) % 1e4
+
+        let messageLimit = hash.v3(`2021-05_premium_increased_content_length:${argMember.id}`) % 1e4
+
+        //respond(`Profile Banner/About Me **[BETA]**\n\n${argMember} position is **\`${position}\`**`)
+
+        respond([
+          `**${argMember.user.tag}** info`,
+          ``,
+          `**\`Custom Profiles Premium\`** - \`Restart/update discord on pc if you dont have.\``,
+          `**\`Banner Color Picker\`** - \`${bannerColorPicker}\` from \`unkown\` per week`,
+          `**\`Per Guild Avatars\`** - \`${perGuildAvatars}\` from \`500/1900\` per week`,
+          `**\`Increased Message Limit\`** - \`${messageLimit}\` from \`unkown\` per week`,
+        ].join("\n"))
     }
 }
