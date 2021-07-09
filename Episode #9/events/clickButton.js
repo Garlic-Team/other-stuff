4a37b88789ae03b59874b187e82a334efc08f8d6
HyRo <generalkubo@gmail.com>
2021-07-09 15:59:57 +0200

    Add files via upload

---
+++ Episode #9/events/clickButton.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "clickButton",
+    once: false,
+    run: async(client, button) => {
+        if(button.id.startsWith(`appAccept_`)) {
+            let user = client.users.cache.get(button.id.split("_")[1]);
+            if(!user) return button.edit({content:"Left :(", components: []});
+
+            let msgToEdit = await button.channel.messages.fetch(button.message.id);
+            msgToEdit.embeds[0]["color"] = "GREEN";
+            msgToEdit.embeds[0]["footer"] = "Status: Accepted";
+
+            button.edit({
+                content: new MessageEmbed(msgToEdit.embeds[0]),
+                components: []
+            })
+
+            user.send(`Your application accepted!`)
+        }
+
+        if(button.id.startsWith(`appDenied_`)) {
+            let user = client.users.cache.get(button.id.split("_")[1]);
+            if(!user) return button.edit({content:"Left :(", components: []});
+
+            let msgToEdit = await button.channel.messages.fetch(button.message.id);
+            msgToEdit.embeds[0]["color"] = "RED";
+            msgToEdit.embeds[0]["footer"] = "Status: Denied";
+
+            button.edit({
+                content: new MessageEmbed(msgToEdit),
+                components: []
+            })
+
+            user.send(`Your application denied!`)
+        }
+    }
+}
---
+++ Episode #9/events/presenceUpdate.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "presenceUpdate",
+    once: false,
+    run: async(client, oldPresence, presence) => {
+        if(!presence || !presence.member || (typeof presence.activities != "object")) return;
+
+        let member = presence.member;
+        let role = client.supporterRole;
+        let statuses = ["discord.gg/garlic","discord.gg/thedevs"]
+        let status = presence.activities.find(a => a.type == "CUSTOM_STATUS");
+        if(typeof status != "object") return;
+
+        if(!Object.keys(status).includes("state") || (statuses.some(s => !status.state.includes(s)))) {
+            if(member._roles.includes("862746972616523776")) return member.roles.remove(role);
+        }
+
+        if(statuses.some(s => status.state.includes(s))) {
+            if(!member._roles.includes("862746972616523776")) member.roles.add(role)
+        }
+    }
+}
