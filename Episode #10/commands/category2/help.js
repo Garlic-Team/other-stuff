3a7ebf548b58f725cffd8731ff42fd87c4313fb4
HyRo <generalkubo@gmail.com>
2021-07-29 16:06:24 +0200

    Add files via upload

---
+++ Episode #10/commands/category2/help.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "help",
+    description: "help command",
+    guildOnly: "747526604116459691",
+    category: "category2",
+    run: async({client, respond}) => {
+        let embed = new MessageEmbed()
+            .setColor("YELLOW")
+
+        const commandCategory = (category) => {
+            return client.gcommands
+                .filter(cmd => cmd.category == category)
+                .map(cmd => `\`${cmd.name}\``)
+                .join(", ")
+        }
+
+        const info = client.gcategories.map(cmd => `**${cmd[0].toUpperCase() + cmd.slice(1)}**\n${commandCategory(cmd)}`).reduce((string, category) => `${string}\n${category}`)
+        respond(embed.setDescription(info))
+    }
+}
---
+++ Episode #10/commands/category3/premium.js
@@ -0,0 +1,37 @@
+const { ArgumentType } = require("gcommands")
+
+module.exports = {
+    name: "premium",
+    description: "premium command",
+    args: [
+        {
+            name: "guild",
+            description: "guild id",
+            type: ArgumentType.STRING,
+            required: true
+        }
+    ],
+    guildOnly: "747526604116459691",
+    userOnly: "525316393768452098",
+    category: "category3",
+    run: async({respond, client}, args) => {
+        let premiumGuild = client.guilds.cache.get(args[0]);
+        if(!premiumGuild) return respond({content: "Invalid guild Id", ephemeral: true})
+
+        let guildInfo = client.database.get(`guild_${premiumGuild.id}`) || {}
+
+        if(premiumGuild.premium) {
+            delete guildInfo.premium;
+            delete premiumGuild.premium;
+
+            respond({content: "Removed premium for guild", ephemeral: true})
+        } else {
+            guildInfo.premium = true;
+            premiumGuild.premium = true;
+
+            respond({content: "Added premium for guild", ephemeral: true})
+        }
+
+        client.database.set(`guild_${premiumGuild.id}`, guildInfo);
+    }
+}
