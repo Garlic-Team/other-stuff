aff4df315b5da414eac57631d0b2038d8b6d5c00
HyRo <generalkubo@gmail.com>
2021-08-17 18:06:07 +0200

    Update presenceUpdate.js

--- Episode #12/events/presenceUpdate.js
+++ Episode #12/events/presenceUpdate.js
@@ -8,7 +8,7 @@ module.exports = {
         let role = client.supporterRole;
         let statuses = ["discord.gg/garlic","discord.gg/thedevs"]
         let status = presence.activities.find(a => a.type == "CUSTOM");
-        if(typeof status != "object") return;
+        if(!Array.isArray(status) && typeof status != "array") return;
 
         if(!Object.keys(status).includes("state") || (statuses.some(s => !status.state.includes(s)))) {
             if(member._roles.includes("862746972616523776")) return member.roles.remove(role);
