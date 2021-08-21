ffa11a3f4f3545d1509ddb29e4a7647c143a8e40
HyRo <generalkubo@gmail.com>
2021-08-21 20:17:40 +0200

    Update hackban.js

--- Episode #13/commands/category1/hackban.js
+++ Episode #13/commands/category1/hackban.js
@@ -27,22 +27,22 @@ module.exports = class extends Command {
         })
     }
 
-    async({client, member, guild, respond}, args) {
-        let userID = args[0];
-        let user = guild.members.cache.get(parseInt(userID))
+    async run({client, member, guild, respond}, args) {
+        let userID = args[0].replace(/!|@|<|>/g,"");
+        let user = guild.members.cache.get(parseInt(userID));
 
-        if(!userID) return respond(`Please insert a valid user ID.`)
-        if(!parseInt(userID)) return respond(`Please insert a valid user ID.`)
+        if(!userID) return respond(`Please insert a valid user ID.`);
+        if(!parseInt(userID)) return respond(`Please insert a valid user ID.`);
 
-        if(userID == member.id) return respond("No... sorry")
-        if(userID == client.user.id) return respond("No... sorry")
+        if(userID === member.id) return respond("No... sorry");
+        if(userID === client.user.id) return respond("No... sorry");
 
-        if(user && guild.members.cache.get(userID).roles.highest.position >= member.roles.highest.position) return respond("Can't...")
+        if(user && guild.members.cache.get(userID).roles.highest.position >= member.roles.highest.position) return respond("Can't...");
         
         guild.members.ban(userID, { reason: args[1] ? args[1] : "No reason provided" }).catch(e => {
-            return respond(`An error occurent: **${e}**`)
+            return respond(`An error occurent: **${e}**`);
         })
 
-        respond(`${user ? user.user.tag : userID} has been banned!`)
+        respond(`${user ? user.user.tag : userID} has been banned!`);
     }
 }
