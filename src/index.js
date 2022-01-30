15378a713e18efbf85adc2b84e623d7cd224f8e4
xhyrom <generalkubo@gmail.com>
2022-01-30 09:54:34 +0100

    base

--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
 require('dotenv').config();
-const { Client, MessageEmbed } = require('discord.js');
-const tiktok = require('tiktok-scraper');
+const { Client } = require('discord.js');
+const RSSParser = require('rss-parser');
 const urlRegexp = /(https?:\/\/[^ ]*)/;
 
 const client = new Client({
     intents: ['GUILDS','GUILD_MESSAGES']
 })
 
-const resolveId = async () => (await client.tiktok.getUserProfileInfo(process.env.TIKTOK_ACCOUNT)).userId;
+const request = new RSSParser();
 
 client.on('ready', async() => {
-    const tiktokChannel = await client.channels.fetch(process.env.NOTIFICATION_CHANNEL_ID);
-    const userID = await resolveId();
-    const user = await tiktok.user(userID);
+    const ytChannel = await client.channels.fetch(process.env.NOTIFICATION_CHANNEL_ID);
 
     setInterval(async() => {
-        const { collector: newPosts } = user;
-        if (newPosts.length === 0) return;
+        const req = (await request.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${process.env.YOUTUBE_CHANNEL_ID}`)).items[0];
 
-        const post = newPosts[0];
-        const author = post.authorMeta.nickName;
-        const link = post.webVideoUrl;
-
-        let ifAlready = [...(await tiktokChannel.messages.fetch({ limit: 1 })).values()];
+        let ifAlready = [...(await ytChannel.messages.fetch({ limit: 1 })).values()]
         if (ifAlready.length > 0) ifAlready = ifAlready[0].content.match(urlRegexp);
         if (ifAlready != null) ifAlready = ifAlready[1];
-        if (ifAlready == link) return;
+        if (ifAlready == req.link) return;
 
-        const embed = new MessageEmbed()
-            .setAuthor({ name: author, iconURL: post.authorMeta.avatar })
-            .setTitle(post.text)
-            .setImage(post.covers.default)
-            .setColor('#7f47cc')
-            .setTimestamp()
-            .setFooter({ text: `Fans: ${new Intl.NumberFormat().format(post.authorMeta.fans)} | Videos: ${new Intl.NumberFormat().format(post.authorMeta.video)} | Hearts: ${new Intl.NumberFormat().format(post.authorMeta.heart)}` });
-
-        tiktokChannel.send({
-            content: process.env.NEW_POST_MESSAGE.replace(`{url}`,link),
-            embeds: [embed]
-        });
+        ytChannel.send(`${process.env.NEW_POST_MESSAGE.replace(`{url}`,req.link)}`);
     }, 15000)
 })
 
