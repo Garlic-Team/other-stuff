a1e55b85a8baf0280382e597fffb2ddfe235b86a
iZboxo <zdenda.havlat@gmail.com>
2022-02-18 15:30:31 +0100

    'PollBot-1.0.1'

--- commands/poll.js
+++ commands/poll.js
@@ -1,8 +1,8 @@
 const { Command, CommandType } = require('gcommands');
 
 new Command({
-	name: 'hello',
-	description: 'Says hello!',
+	name: 'poll',
+	description: 'Create new poll!',
 	type: [CommandType.SLASH, CommandType.MESSAGE],
 	run: (ctx) => {
 		return ctx.reply(`Hello ${ctx.user.username}!`);
