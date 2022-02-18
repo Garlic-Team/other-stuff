6f8d36dc51ff6a90c9b47399bba9fc10ba132a66
iZboxo <zdenda.havlat@gmail.com>
2022-02-18 15:30:14 +0100

    'PollBot-1.0.1'

--- commands/poll.js
+++ commands/poll.js
@@ -4,7 +4,6 @@ new Command({
 	name: 'hello',
 	description: 'Says hello!',
 	type: [CommandType.SLASH, CommandType.MESSAGE],
-	// The function thats executed when the user uses the command.
 	run: (ctx) => {
 		return ctx.reply(`Hello ${ctx.user.username}!`);
 	}
